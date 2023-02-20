a93c2fb6505039536e760f5967f20ba366bd2d45
Gil Alves <gildson_af@outlook.com>
2023-02-19 23:52:47 -0300

    fix: Resolves bug storybook deploy

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,15 +1,14 @@
-import { themes } from "@storybook/theming";
-import "../src/styles/global.css";
+import { themes } from '@storybook/theming'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  docs: {
-    theme: themes.dark,
-  },
-};
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	docs: {
+		theme: themes.dark,
+	},
+}
