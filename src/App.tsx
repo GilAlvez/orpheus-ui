8292465afe9b1ddbdf021a3e24bda2c210edea7d
Gil Alves <gildson_af@outlook.com>
2023-02-19 23:53:53 -0300

    style: Formats App and main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function App() {
-  const [message, setMessage] = useState("");
-  return (
-    <main>
-      <h1>{message}</h1>
-      <button
-        data-testid="button__show-message"
-        onClick={() => setMessage("Hello World")}
-      />
-    </main>
-  );
+	const [message, setMessage] = useState('')
+	return (
+		<main>
+			<h1>{message}</h1>
+			<button data-testid="button__show-message" onClick={() => setMessage('Hello World')} />
+		</main>
+	)
 }
 
-export default App;
+export default App
