c162fd8e5b2bfa0a22b86faa7041d6ef869102ac
Gil Alves <gildson_af@outlook.com>
2023-02-19 23:42:55 -0300

    style: Adds Prettier and ESLint config for code formatting
    This commit adds Prettier and ESLint to the project to ensure consistent and well-formatted code. Prettier will be used for code formatting, while ESLint will help catch code errors and enforce code style rules. The project's codebase is now more maintainable and easier to work with.

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,39 +1,38 @@
-import {createStitches, defaultThemeMap} from '@stitches/react'
+import { createStitches, defaultThemeMap } from '@stitches/react'
 
-import * as utils from './utils'
 import * as theme from './theme'
+import * as utils from './utils'
 
 export const { styled, globalCss, createTheme, getCssText } = createStitches({
-  media: theme.screens,
-  theme: {
-    colors: theme.colors,
-    space: theme.space,
-    sizes: theme.sizes,
-    fonts: theme.fonts,
-    fontSizes: theme.fontSizes,
-    radii: theme.radii,
-    borderWidths: theme.borderWidths,
-    shadows: theme.shadows,
-    fontWeights: theme.fontWeights,
-    lineHeights: theme.lineHeights,
-    zIndices: theme.zIndices,
+	media: theme.screens,
+	theme: {
+		colors: theme.colors,
+		space: theme.space,
+		sizes: theme.sizes,
+		fonts: theme.fonts,
+		fontSizes: theme.fontSizes,
+		radii: theme.radii,
+		borderWidths: theme.borderWidths,
+		shadows: theme.shadows,
+		fontWeights: theme.fontWeights,
+		lineHeights: theme.lineHeights,
+		zIndices: theme.zIndices,
 
-    extendMargin: theme.extendMargin
-  },
-  utils: {
-    ...utils
-  },
-  themeMap: {
-    ...defaultThemeMap,
-    margin: 'extendMargin',
-  }
+		extendMargin: theme.extendMargin,
+	},
+	utils: {
+		...utils,
+	},
+	themeMap: {
+		...defaultThemeMap,
+		margin: 'extendMargin',
+	},
 })
 
 export const dark = 'html.dark &' // CSS selector for dark theme
 createTheme('dark', {
-  ...theme
+	...theme,
 })
-
 
 /* globalCss({
   '@import': 'normalize.css',
