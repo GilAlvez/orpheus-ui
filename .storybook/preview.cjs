fe810421648c8d7d02108ee02d0142a5a8046803
Gil Alves <gildson_af@outlook.com>
2023-02-23 18:12:31 -0300

    feat: Adds fontsource lib to handle fonts

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,7 +1,14 @@
 import { themes } from '@storybook/theming'
 import { globalStyles } from '../src/styles/stitches.config'
 
+import '@fontsource/inter/300.css'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
+
 globalStyles()
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
