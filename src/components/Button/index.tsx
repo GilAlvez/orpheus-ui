1e7f61401b9911eb61f585f27d813c679ccad3b3
Gil Alves <gildson_af@outlook.com>
2023-02-24 18:45:38 -0300

    feat: Adds Button component initial config

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import { type IntrinsicElementsKeys } from '@stitches/react/types/styled-component'
+import { forwardRef, type ButtonHTMLAttributes, type ForwardedRef, type ReactNode } from 'react'
+import * as S from './styles'
+
+export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning' | 'dark' | 'light'
+	size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+	variant?: 'filled' | 'outlined' | 'text'
+	fullWidth?: boolean
+	as: IntrinsicElementsKeys | ReactNode
+	iconStart?: ReactNode
+	iconEnd?: ReactNode
+}
+
+export const Button = forwardRef(function Button(props: IButtonProps, ref: ForwardedRef<HTMLButtonElement>) {
+	const { children, iconStart, iconEnd, ...rest } = props
+
+	return (
+		<S.Button ref={ref} {...rest}>
+			{iconStart && <S.Icon>{iconStart}</S.Icon>}
+			{children}
+			{iconEnd && <S.Icon>{iconEnd}</S.Icon>}
+		</S.Button>
+	)
+})
