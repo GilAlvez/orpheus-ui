57146f56001a058e7f3758ba3b1c3f1bcc39a2f3
Gil Alves <gildson_af@outlook.com>
2023-02-26 21:23:13 -0300

    feat: Adds initial Storybook setup of Button

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -1,21 +1,75 @@
 import { type Meta, type StoryObj } from '@storybook/react'
 import { Button, type IButtonProps } from '.'
 
+import { Plus } from 'phosphor-react'
+
 export default {
 	title: 'Components/Button',
 	component: Button,
-	/* args: {
-		children: 'Lorem ipsum.',
+
+	// Default
+	args: {
+		color: 'primary',
 		size: 'md',
+		variant: 'filled',
 	},
+
+	// Panel
 	argTypes: {
+		children: {
+			name: 'text',
+		},
+		color: {
+			options: ['primary', 'secondary', 'success', 'error', 'warning'],
+			control: {
+				type: 'select',
+			},
+		},
 		size: {
-			options: ['sm', 'md', 'lg'],
+			options: ['xs', 'sm', 'md', 'lg', 'xl'],
 			control: {
 				type: 'inline-radio',
 			},
 		},
-	}, */
+		variant: {
+			options: ['filled', 'outlined', 'text'],
+			control: {
+				type: 'inline-radio',
+			},
+		},
+		disabled: {
+			defaultValue: false,
+			control: {
+				type: 'boolean',
+			},
+		},
+		fullWidth: {
+			defaultValue: false,
+			control: {
+				type: 'boolean',
+			},
+		},
+		as: {
+			table: {
+				disable: true,
+			},
+		},
+		css: {
+			table: {
+				disable: true,
+			},
+		},
+		iconStart: {
+			table: {
+				disable: true,
+			},
+		},
+		iconEnd: {
+			table: {
+				disable: true,
+			},
+		},
+	},
 } as Meta<IButtonProps>
 
 export const Default: StoryObj<IButtonProps> = {
@@ -24,33 +78,25 @@ export const Default: StoryObj<IButtonProps> = {
 	},
 }
 
-/* export const Small: StoryObj<IButtonProps> = {
+export const AsLink: StoryObj<IButtonProps> = {
+	name: 'As Link',
 	args: {
-		size: 'sm',
-	},
-}
-
-export const Large: StoryObj<IButtonProps> = {
-	args: {
-		size: 'lg',
-	},
-}
-
-export const CustomComponent: StoryObj<IButtonProps> = {
-	args: {
-		asChild: true,
-		children: <p>Text with P tag</p>,
+		as: 'a',
+		children: 'As Link',
 	},
 	argTypes: {
-		children: {
+		as: {
 			table: {
-				disable: true,
-			},
-		},
-		asChild: {
-			table: {
-				disable: true,
+				disable: false,
 			},
 		},
 	},
-} */
+}
+
+export const WithIcon: StoryObj<IButtonProps> = {
+	name: 'With Icons',
+	args: {
+		children: 'With Icon',
+		iconStart: <Plus />,
+	},
+}
