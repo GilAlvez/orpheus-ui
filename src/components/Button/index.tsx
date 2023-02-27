0a9233535e686235641e4cacb1ed210339a69037
Gil Alves <gildson_af@outlook.com>
2023-02-26 21:15:26 -0300

    refact: Updates Button type w/ stitches component props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,22 @@
-import { type IntrinsicElementsKeys } from '@stitches/react/types/styled-component'
-import { forwardRef, type ButtonHTMLAttributes, type ForwardedRef, type ReactNode } from 'react'
+import { type ComponentProps } from '@stitches/react'
+import { type ElementType, type ReactNode } from 'react'
+
 import * as S from './styles'
 
-export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning'
-	size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
-	variant?: 'filled' | 'outlined' | 'text'
-	fullWidth?: boolean
-	as: IntrinsicElementsKeys | ReactNode
+export interface IButtonProps extends ComponentProps<typeof S.Button> {
+	as?: ElementType
 	iconStart?: ReactNode
 	iconEnd?: ReactNode
 }
 
-export const Button = forwardRef(function Button(props: IButtonProps, ref: ForwardedRef<HTMLButtonElement>) {
+export const Button = (props: IButtonProps) => {
 	const { children, iconStart, iconEnd, ...rest } = props
 
 	return (
-		<S.Button ref={ref} {...rest}>
+		<S.Button tabIndex={0} {...rest}>
 			{iconStart && <S.Icon>{iconStart}</S.Icon>}
 			{children}
 			{iconEnd && <S.Icon>{iconEnd}</S.Icon>}
 		</S.Button>
 	)
-})
+}
