308a45430369650b7799a0959d64708a9d87e3c6
Gil Alves <gildson_af@outlook.com>
2023-02-28 17:38:41 -0300

    feat: Adds key press handler to polymorphic Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,20 +1,27 @@
 import { type ComponentProps } from '@stitches/react'
-import { type ElementType, type ReactNode } from 'react'
+import { useRef, type ElementType, type ReactNode } from 'react'
 
 import * as S from './styles'
 
 export interface IButtonProps extends ComponentProps<typeof S.Button> {
-	size: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 	as?: ElementType
 	icon?: ReactNode
 }
 
 export const Button = (props: IButtonProps) => {
-	const { children, icon, size, ...rest } = props
+	const { children, icon, onKeyDown, ref, ...rest } = props
+
+	const buttonRef = useRef<HTMLButtonElement>(null)
+
+	const handleKeyPress = (e: React.KeyboardEvent<HTMLButtonElement>) => {
+		if (e.key === 'Enter' || e.key === ' ') {
+			buttonRef.current?.click()
+		}
+	}
 
 	return (
-		<S.Button hasIcon={!!icon} size={size} tabIndex={0} {...rest}>
-			{icon && <S.Icon size={size}>{icon}</S.Icon>}
+		<S.Button hasIcon={!!icon} ref={ref ?? buttonRef} onKeyDown={onKeyDown ?? handleKeyPress} tabIndex={0} {...rest}>
+			{icon && <S.Icon size={props.size}>{icon}</S.Icon>}
 			{children}
 		</S.Button>
 	)
