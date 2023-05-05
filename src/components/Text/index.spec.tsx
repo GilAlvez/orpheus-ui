d0d42b5b2905d83000fb010dbd1a798c1c0d6b28
Gil Alves <gildson_af@outlook.com>
2023-05-04 21:13:11 -0300

    test: Improves `Text` subcomponents validation

--- src/components/Text/index.spec.tsx
+++ src/components/Text/index.spec.tsx
@@ -1,31 +1,71 @@
-import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-import { describe, test } from 'vitest'
-import { Text } from '.'
-import { Body } from './styles'
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import { describe, test } from 'vitest';
+import { Text } from '.';
+import { textBase } from './styles';
+
+function testTextComponent(componentName: string, Component: any) {
+	describe(componentName, () => {
+		let textElement: ChildNode | null;
+
+		function renderTextComponent({ size, weight }: { size?: string; weight?: string }): ChildNode | null {
+			const { container } = render(
+				<Component size={size} weight={weight}>
+					{componentName}
+				</Component>
+			);
+			const textElement = container.firstChild;
+			return textElement;
+		}
+		beforeEach(() => {
+			textElement = renderTextComponent({});
+		});
+
+		test('should render with correct text content', () => {
+			// Assert
+			expect(textElement).toHaveTextContent(componentName);
+		});
+
+		test('should have the correct base className', () => {
+			// Arrange
+			const textHTMLElement = textElement as HTMLElement;
+
+			// Assert
+			expect(textHTMLElement.classList.contains(textBase.className)).toBeTruthy();
+		});
+
+		test('should have the correct default prop in `size`', () => {
+			// Assert
+			expect(textElement).toHaveAttribute('class', expect.stringContaining('size-md'));
+		});
+
+		test('should have the correct `size` value when prop is declared', () => {
+			// Arrange
+			textElement = renderTextComponent({ size: 'lg' });
+
+			// Assert
+			expect(textElement).toHaveAttribute('class', expect.stringContaining('size-lg'));
+		});
+
+		test('should have the correct default prop in `weight`', () => {
+			// Assert
+			expect(textElement).toHaveAttribute('class', expect.stringContaining('weight-normal'));
+		});
+
+		test('should have the correct `weight` value when prop is declared', () => {
+			// Arrange
+			textElement = renderTextComponent({ weight: 'bold' });
+
+			// Assert
+			expect(textElement).toHaveAttribute('class', expect.stringContaining('weight-bold'));
+		});
+	});
+}
 
 describe('Text', () => {
-	test('should renders with correct text content', () => {
-		const { container } = render(<Text.Body>Body</Text.Body>)
-		const bodyText = container.firstChild
-		expect(bodyText).toHaveTextContent('Body')
-	})
-
-	test('should have the correct className', () => {
-		const { container } = render(<Text.Body>Body</Text.Body>)
-		const bodyText = container.firstChild as HTMLElement
-		expect(bodyText.classList.contains(Body.className)).toBeTruthy()
-	})
-
-	test('should have the correct default prop in `size`', () => {
-		const { container } = render(<Text.Body>Body</Text.Body>)
-		const bodyText = container.firstChild
-		expect(bodyText).toHaveAttribute('class', expect.stringContaining('size-md'))
-	})
-
-	test('should have the correct default prop in `weight`', () => {
-		const { container } = render(<Text.Body>Body</Text.Body>)
-		const bodyText = container.firstChild
-		expect(bodyText).toHaveAttribute('class', expect.stringContaining('weight-normal'))
-	})
-})
+	testTextComponent('Display', Text.Display);
+	testTextComponent('Heading', Text.Heading);
+	testTextComponent('Title', Text.Title);
+	testTextComponent('Label', Text.Label);
+	testTextComponent('Body', Text.Body);
+});
