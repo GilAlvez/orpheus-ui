8db9cddb9c898f8f26e0de6068401884627ee19d
Gil Alves <gildson_af@outlook.com>
2023-05-14 09:37:36 -0300

    ci: Fixes bug in merged packages changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,5 @@ export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	presets: [require('./src/config/orpheus.preset.js')],
 	theme: {},
-<<<<<<< Updated upstream
-	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
-=======
 	plugins: [],
->>>>>>> Stashed changes
 };
